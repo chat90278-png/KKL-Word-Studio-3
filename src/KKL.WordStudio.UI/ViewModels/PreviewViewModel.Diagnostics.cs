f926e855e57b38f27b60b03439e64c9ae6c2678a
chat90278-png <chat90278@gmail.com>
2026-07-11 12:33:43 +0300

    Keep preview diagnostics partial focused on navigation

--- src/KKL.WordStudio.UI/ViewModels/PreviewViewModel.Diagnostics.cs
+++ src/KKL.WordStudio.UI/ViewModels/PreviewViewModel.Diagnostics.cs
@@ -1,37 +1,12 @@
 namespace KKL.WordStudio.UI.ViewModels;
 
-using System.Collections.ObjectModel;
-using CommunityToolkit.Mvvm.Input;
-using KKL.WordStudio.Application.Preview;
-
 public sealed partial class PreviewViewModel
 {
-    public ObservableCollection<PreviewDiagnostic> Diagnostics { get; } = new();
-
-    public int WarningCount => Diagnostics.Count;
-    public bool HasWarnings => WarningCount > 0;
-    public string WarningCountText => WarningCount == 1 ? "1 uyarı" : $"{WarningCount} uyarı";
-
-    public event Action? OpenWarningsRequested;
     public event Action<Guid>? NavigateToElementRequested;
-
-    [RelayCommand]
-    private void OpenWarnings() => OpenWarningsRequested?.Invoke();
 
     public void NavigateToElement(Guid elementId)
     {
         _workspace.SetSelectedReportElement(elementId);
         NavigateToElementRequested?.Invoke(elementId);
-    }
-
-    private void ReplaceDiagnostics(IReadOnlyList<PreviewDiagnostic> diagnostics)
-    {
-        Diagnostics.Clear();
-        foreach (var diagnostic in diagnostics)
-            Diagnostics.Add(diagnostic);
-
-        OnPropertyChanged(nameof(WarningCount));
-        OnPropertyChanged(nameof(HasWarnings));
-        OnPropertyChanged(nameof(WarningCountText));
     }
 }
