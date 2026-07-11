864c9dc768bddf699be45ba87e5d7e5454e18c5d
chat90278-png <chat90278@gmail.com>
2026-07-11 09:45:04 +0300

    Revert speculative automatic width adaptation

--- src/KKL.WordStudio.Application/Formatting/ReferenceReportContentFormatResolver.cs
+++ src/KKL.WordStudio.Application/Formatting/ReferenceReportContentFormatResolver.cs
@@ -65,10 +65,8 @@ public sealed class ReferenceReportContentFormatResolver : IReportContentFormatR
 
         var effectiveProfile = profile ?? DefaultDocumentFormatProfileFactory.Create();
         var selected = TableFormatProfileSelector.Select(effectiveProfile, table);
-        if (selected is null)
-            return compatibilityResolver.ResolveTable(null, table);
 
-        return AutomaticTableFormatWidthAdapter.Adapt(selected, table);
+        return selected?.Format ?? compatibilityResolver.ResolveTable(null, table);
     }
 
     public PageLayout ResolvePageLayout(
