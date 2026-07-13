dda58c8017c686ca494e4e948737b8ad6d57db7c
chat90278-png <chat90278@gmail.com>
2026-07-13 17:11:04 +0300

    Sprint 23: complete OpenXML test imports

--- tests/KKL.WordStudio.Infrastructure.Tests/Sprint23ExcelReaderCharacterizationTests.cs
+++ tests/KKL.WordStudio.Infrastructure.Tests/Sprint23ExcelReaderCharacterizationTests.cs
@@ -1,5 +1,6 @@
 namespace KKL.WordStudio.Infrastructure.Tests;
 
+using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using KKL.WordStudio.Infrastructure.Excel;
