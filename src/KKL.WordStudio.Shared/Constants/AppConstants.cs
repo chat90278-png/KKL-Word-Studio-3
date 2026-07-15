a780a6d5e805320a134d4c4b5f6505fea464b206
chat90278-png <chat90278@gmail.com>
2026-07-15 11:37:15 +0300

    Remove native project file constants

--- src/KKL.WordStudio.Shared/Constants/AppConstants.cs
+++ src/KKL.WordStudio.Shared/Constants/AppConstants.cs
@@ -4,9 +4,4 @@ namespace KKL.WordStudio.Shared.Constants;
 public static class AppConstants
 {
     public const string ApplicationName = "KKL Word Studio";
-
-    /// <summary>File extension for the native project container (see ADR 0001).</summary>
-    public const string ProjectFileExtension = ".kws";
-
-    public const string ProjectFileFormatVersion = "1.0";
 }
